f577ccf0c3fc93478b8ec05f27b5c5c809551f03
akbarjane <501123445a@gmail.com>
2020-09-21 22:45:50 +0500

    first commit

--- Methods.ts
+++
@@ -1,6 +0,0 @@
-export function Parse<T>(tempArr : Array<T>){
-    var props = Object.keys(tempArr[0]) as (keyof T)[]
-    return tempArr.Select(props) as unknown as T[] ;
-}
-export type obj<T> =  {[P in keyof T]?: T[P] | string}
- 
